f2e7b775fb3f9201221589f90dee6efeee96d7a2
logan026 <logan-pike@live.nmit.ac.nz>
2026-05-20 18:21:58 +1200

    Added CLI menu loop.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //Initialize the readLine interface
-const readline = required('node:readline/promises');
-const { stdin: input, stdout: output } = required('node:process');
+const readline = require('node:readline/promises');
+const { stdin: input, stdout: output } = require('node:process');
+function createInterface() {
+    return readline.createInterface({ input, output });
+};
 
-//Array to store our study session in
+//Array to store our study session's in
 const studySessions = [];
+
+//Creatomg a menu to input study data
+async function addStudyMenu() {
+    const rl = createInterface();
+};
