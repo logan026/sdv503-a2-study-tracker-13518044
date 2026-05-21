8c2c1a60ee6d914e26d3b8abc509b1be62931367
logan026 <logan-pike@live.nmit.ac.nz>
2026-05-21 12:25:05 +1200

    Added topic validation.

--- index.js
+++ index.js
@@ -2,31 +2,29 @@
 const readline = require('node:readline/promises');
 const { stdin: input, stdout: output } = require('node:process');
 function createInterface() {
-    return readline.createInterface({ input, output });
+    return readline.createInterface({ input, output }); //Tells programm to wait for input before outputting to the terminal
 };
 
 //Array to store our study session's in
 const studySessions = [];
 
-//Creatomg a menu to input study data
+//Creating a menu to input study data
 async function addStudyMenu() {
-    const rl = createInterface();
-    console.log('\n----- Add New Study Session -----');
+    const rl = createInterface(); //Creates function to handle user input and ouput
+    console.log('\n----- Add New Study Session -----'); //\n creates a new line for the text to be put on ensuring our CLI stays clean
     //Topic input loop
     try { 
         const topic = await rl.question('Enter Study Topic: '); //Topic prompt
         if (!topic.trim()) { //Checks if input was empty or not
             console.log('Error: Topic cannot be empty.'); //Throws an error message if input was empty
-            rl.close(); //Clsoes current loop
-            return await addStudyMenu(); //Sends user to start of loop after error
+            rl.close(); //Closes channel for user input
+            await addStudyMenu(); //Sends user to start of loop after error
+            return;
         }
-        rl.close();
-        await console.log(topic); //Prints the topic we chose to the console for testing
-        return topic;
+        console.log('Topic Added: ' + topic); //Shows the user the topic they added after succesful input
     } finally {
-        
-    } 
+        rl.close(); //Closes the loop
+    }
 };
-addStudyMenu();
 
-
+addStudyMenu(); //Using function for testing
